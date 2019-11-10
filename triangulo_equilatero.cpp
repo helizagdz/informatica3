c3cb385c93e7c00af1aeb93017671abf4b8bc4a0
Sergio Gtz <sergio.oscar.gb@gmail.com>
2019-11-10 13:04:24 -0600

    programa triangulo equilatero

---
+++ triangulo_equilatero.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(void) {
+     int num1,num2,num3;
+  	printf("Dar el primer lado: ");
+	scanf("%d",&num1);
+	printf("Dar el segundo lado: ");
+	scanf("%d",&num2);
+    printf("Dar el tercer lado: ");
+	scanf("%d",&num3);
+
+    if((num1==num2) && (num2==num3)){
+        printf ("Es un triangulo equilatero") ;
+    }
+    else {
+       printf ("No es un triangulo equilatero") ;
+    }
+
+    printf ("\n");
+}
